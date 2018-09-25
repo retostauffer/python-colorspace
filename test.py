602bd6a5714bd786b3c38921b063fecdd395d26c
Reto Stauffer <reto.stauffer@uibk.ac.at>
2018-09-25 17:55:20 +0200

    Travis test

---
+++ test.py
@@ -0,0 +1,16 @@
+
+# Loading package
+from colorspace import *
+from colorspace.colorlib import *
+
+# Generate a set of colors
+cols = hexcols(["#023FA5", "#A1A6C8", "#E2E2E2", "#CA9CA4", "#8E063B"])
+
+# Convert colors
+cols.to("HCL")
+print(cols)
+
+cols.to("hex")
+print(cols)
+
+cols.swatchplot()
