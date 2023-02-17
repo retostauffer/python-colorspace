c92e4c937ff29025d45bfc5f79c6c9444dc75d0a
= <Reto.Stauffer@uibk.ac.at>
2023-02-17 11:58:32 +0100

    fix: Still troubleshooting html_logo option of sphinx when rendered via github actions

--- source/conf.py
+++ source/conf.py
@@ -231,7 +231,7 @@ html_sidebars = {"**": ["localtoc.html", "sourcelink.html", "searchbox.html"]}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 # -- Autodoc configuration
 
